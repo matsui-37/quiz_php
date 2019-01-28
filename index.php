7a3301a881e11b1a8527523641128c182176ab42
matsui-37 <matsuigyouhiroshi@matsui-mbp.local>
2019-01-28 23:03:11 +0900

    page-design

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>Quiz</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <div id="container">
+      <h1>Q. What is A?</h1>
+      <ul>
+        <li class="answer">A0</li>
+        <li class="answer">A1</li>
+        <li class="answer">A2</li>
+        <li class="answer">A3</li>
+      </ul>
+      <div id="btn" class="disabled">Next Question</div>
+    </div>
+  </body>
+</html>
+
